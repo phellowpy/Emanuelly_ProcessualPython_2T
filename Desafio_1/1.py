976a7b2829bd46c488d06b26c8bde4e3887be494
Emanuelly Sena <147276660+phellowpy@users.noreply.github.com>
2025-05-23 09:53:17 -0300

    desafio 1

---
+++ Desafio_1/1.py
@@ -0,0 +1,9 @@
+# entrada de dados
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+genero = str(input("Digite seu genero: "))
+cidade = str(input("Digite sua cidade: "))
+profissao = str(input("Digite sua profissao: "))
+#saida de dados
+print(f"\nOla {nome}, bem vindo(a)!")
+print(f"Voce tem {idade} anos, possui o genero {genero}, mora em {cidade} e trabalha como {profissao}.")
